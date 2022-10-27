549d438c4b6b7436feea2fa54a5d7844ec08f999
suan <sue9340@gmail.com>
2022-10-27 20:25:12 +0900

    추가
    맞았을 때 메테리얼 바꾸게

--- Assets/03_Scripts/Player/PlayerController.cs
+++ Assets/03_Scripts/Player/PlayerController.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody myrigid;
-    public Material material;
+    private Renderer render;
     private bool isJump = false;
 
     [Header("MoveSpeed")]
@@ -22,12 +23,24 @@ public class PlayerController : MonoBehaviour
     [Header("Player Life")]
     public float playerLife;
 
+    [Header("PlayerDamaged ChangeColor Alpha")]
+    public float alphaColr;
+
+    public Color changeColor;
+    private Color defaultColor;
+
     private bool isDamaged = false;
 
-    private readonly WaitForSeconds damageSpriteTime = new WaitForSeconds(0.1f);
+    private readonly WaitForSeconds damageSpriteTime = new WaitForSeconds(0.15f);
     private void Start()
     {
         myrigid = GetComponent<Rigidbody>();
+        render = GetComponentInChildren<Renderer>();
+
+
+
+        defaultColor = render.material.color;
+        changeColor.a = alphaColr;
     }
 
     private void Update()
@@ -94,8 +107,17 @@ public class PlayerController : MonoBehaviour
 
         playerLife--;
 
+
         int _dirc = transform.position.x - _targetPos.x > 0 ? 1 : -1;
         myrigid.AddForce(new Vector3(_dirc, 1f, 0f) * bouncePower, ForceMode.Impulse);
+
+        for (int i = 0; i < 4; i++)
+        {
+            yield return damageSpriteTime;
+            render.material.color = changeColor;
+            yield return damageSpriteTime;
+            render.material.color = defaultColor;
+        }
 
         yield break;
     }
