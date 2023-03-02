abcafc630fe63ccc92fa05755a669d41c83ac981
suan <sue9340@gmail.com>
2023-03-03 00:17:13 +0900

    개복치랑 돌멩이애니메이션 설정

---
+++ Life in music/Assets/02_Scripts/Rhythm/Stage_1/RockMove.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockMove : MonoBehaviour
+{
+    public enum Type
+    {
+        rock,
+        fish,
+    }
+
+    public Type type;
+}
