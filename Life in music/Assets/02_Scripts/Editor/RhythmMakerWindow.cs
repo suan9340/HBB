e3e4fdc7ad51e3259ddd2850643a992c0beb7be6
suan <sue9340@gmail.com>
2023-03-07 01:23:35 +0900

    에디터 편집 조금한거

--- Life in music/Assets/02_Scripts/Editor/RhythmMakerWindow.cs
+++ Life in music/Assets/02_Scripts/Editor/RhythmMakerWindow.cs
@@ -125,6 +125,7 @@ public class RhythmMaker : EditorWindow
             if (EditorGUI.EndChangeCheck())
             {
                 myData.MusicPlayTime = musicPlayTimeSec;
+                CalculateNoteCount();
             }
 
 
