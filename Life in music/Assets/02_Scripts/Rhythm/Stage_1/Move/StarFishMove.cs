eaca753b9dec2dbf8a0e0176b8464a634a74235e
shin0411a@naver.com <shin0411a@naver.com>
2023-03-14 01:46:12 +0900

    불가사리 리듬 노트에 맞춰서 나오는 것

--- Life in music/Assets/02_Scripts/Rhythm/Stage_1/Move/StarFishMove.cs
+++ Life in music/Assets/02_Scripts/Rhythm/Stage_1/Move/StarFishMove.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class StarFishMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameObject mom;
+    public static float _spos = -220f;
+
+    public static bool isFirst = true;
+
+    public static void StarFishAdd ()
     {
-        
+        _spos += 20;
+        if (mom == null)
+        {
+            mom = GameObject.Find("Rhythm (Starfish)(Clone)");
+        }
+
+        var _obj = Resources.Load<StarFishMove>("Notes/Stage_01/StarfishNote");
+
+        _spos += 20;
+
+        if (_obj != null)
+        {
+            var _inst = Instantiate(_obj, mom.transform, false);
+            _inst.transform.localPosition = new Vector3(500f, _spos, 0f);
+        }
+        else
+        {
+            Debug.LogError("ShellfishNote NULL");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void Remove()
     {
-        
+        // 자동삭제
+    }
+
+    [Header("NoteAnimation")]
+    public Animator starfish_noteAnimation = null;
+
+    private RectTransform rect;
+
+
+
+    private void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        starfish_noteAnimation = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        MoveStarFish();
+    }
+
+    private void MoveStarFish()
+    {
+        if (isFirst)
+        {
+            RhythmManager.Instance.StartMusic();
+            isFirst = false;
+        }
+    }
+
+    public void ShellfishDown()
+    {
+      //  starfish_noteAnimation.SetTrigger("isDown");
     }
 }
