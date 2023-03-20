efaa98ceb6b403688cbc6309eecd699525e33002
suan <sue9340@gmail.com>
2023-03-20 12:34:05 +0900

    문 누르면 나가짐

--- Life in music/Assets/02_Scripts/Utls/MenuUIManager.cs
+++ Life in music/Assets/02_Scripts/Utls/MenuUIManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuUIManager : MonoBehaviour
 {
     [Header("GameOutUI")]
     public Animator GameOutAnim = null;
+    public List<GameObject> doors = new List<GameObject>();
     private bool isOut = false;
     public void OnClickDoorExit()
     {
@@ -21,6 +23,8 @@ public class MenuUIManager : MonoBehaviour
     {
         Debug.Log("No out");
         OutUI();
+
+
     }
 
     public void OutUI()
@@ -36,10 +40,18 @@ public class MenuUIManager : MonoBehaviour
         if (isOut)
         {
             GameOutAnim.SetBool("isGameOut", true);
+
+            doors[0].SetActive(true);
+            doors[1].SetActive(true);
         }
         else
         {
             GameOutAnim.SetBool("isGameOut", false);
+
+
+
+            doors[0].SetActive(false);
+            doors[1].SetActive(false);
         }
     }
 
