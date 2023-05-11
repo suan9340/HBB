3737e51d67ff75da257757fe5de5f21fcbbb053c
suan <sue9340@gmail.com>
2023-05-11 23:41:04 +0900

    .

--- Life in music/Assets/02_Scripts/Room/BoardController.cs
+++ Life in music/Assets/02_Scripts/Room/BoardController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoardController : MenuControllerMOM
+public class BoardController : MonoBehaviour
 {
 
 }
