dbe0400502088356a4b5caed8334d24b8c9cae79
suan <sue9340@gmail.com>
2023-05-17 23:43:04 +0900

    노드 자동 삭제

--- Life in music/Assets/02_Scripts/Note/NoteRightMove.cs
+++ Life in music/Assets/02_Scripts/Note/NoteRightMove.cs
@@ -8,6 +8,8 @@ public class NoteRightMove : MonoBehaviour
 
     private RectTransform rect;
 
+    private bool isClear = false;
+
     private void Start()
     {
         rect = GetComponent<RectTransform>();
@@ -15,6 +17,15 @@ public class NoteRightMove : MonoBehaviour
 
     private void Update()
     {
+        if (isClear)
+            return;
+
+        if (rect.anchoredPosition.x >= 1800f)
+        {
+            isClear = true;
+            EventManager.TriggerEvent(ConstantManager.NOTE_LIST_REMOVE);
+        }
+
         rect.anchoredPosition += new Vector2(10, 0) * moveSpeed * Time.deltaTime;
     }
 }
